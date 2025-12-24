454079b4f94eae61ffba71d577ccbe08ef1e2883
kashadeveloper <l52282195@gmail.com>
2025-12-24 19:21:41 +0300

    relocated logic to analyzer and scrape filepath with scanner (user input)

--- src/main/java/com/studying/bankAnalyzer/Main.java
+++ src/main/java/com/studying/bankAnalyzer/Main.java
@@ -1,59 +1,46 @@
 package com.studying.bankAnalyzer;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var transactions = Analyzer.ReadTransactions("data.csv");
-        System.out.println("========= ТРАНЗАКЦИИ ==========\n");
-        for (var transaction : transactions) {
-            System.out.println(transaction);
-        }
-        System.out.println("Итого: " + transactions.stream().mapToDouble(Transaction::getAmount).sum());
-        System.out.println("Количество транзакций: " + transactions.size());
-
-        System.out.println("\n========= ТРАНЗАКЦИИ ==========");
-
-        System.out.println("\n=========== ТРАТЫ ПО МЕСЯЦАМ =========");
-        var groupedInfo = groupByPeriod(transactions);
-
-        for (var group : groupedInfo.keySet()) {
-            System.out.println(group + ": " + groupedInfo.get(group));
-        }
-        System.out.println("============= ТРАТЫ ПО МЕСЯЦАМ ==========");
-
-        System.out.println("\n============= КАТЕГОРИИ ТРАТ =============");
-
-        var categories = groupByCategory(transactions);
-        for(var category : categories.keySet()) {
-            System.out.println(category + ": " + categories.get(category));
-        }
-
-        System.out.println("============= КАТЕГОРИИ ТРАТ =============");
-
-        System.out.println("\n\nСАМАЯ ДОРОГАЯ ТРАТА: ");
-        System.out.println(getMostExpensive(transactions));
-    }
-
-    public static HashMap<String, Integer> groupByPeriod(List<Transaction> transactions) {
-        var result = new HashMap<String, Integer>();
-        for (var transaction : transactions) {
-            var period = transaction.Date.getMonth().toString() + " " + transaction.Date.getYear();
-            if (result.containsKey(period)) {
-                result.put(period, result.get(period) + 1);
-            } else {
-                result.put(period, 1);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите путь до файла: ");
+        var filePath = sc.nextLine();
+        try {
+            var transactions = Analyzer.ReadTransactions(filePath);
+            System.out.println("========= ТРАНЗАКЦИИ ==========\n");
+            for (var transaction : transactions) {
+                System.out.println(transaction);
             }
+            System.out.println("Итого: " + transactions.stream().mapToDouble(Transaction::getAmount).sum());
+            System.out.println("Количество транзакций: " + transactions.size());
+
+            System.out.println("\n========= ТРАНЗАКЦИИ ==========");
+
+            System.out.println("\n=========== ТРАТЫ ПО МЕСЯЦАМ =========");
+            var groupedInfo = Analyzer.groupByPeriod(transactions);
+
+            for (var group : groupedInfo.keySet()) {
+                System.out.println(group + ": " + groupedInfo.get(group));
+            }
+            System.out.println("============= ТРАТЫ ПО МЕСЯЦАМ ==========");
+
+            System.out.println("\n============= КАТЕГОРИИ ТРАТ =============");
+
+            var categories = Analyzer.groupByCategory(transactions);
+            for(var category : categories.keySet()) {
+                System.out.println(category + ": " + categories.get(category));
+            }
+
+            System.out.println("============= КАТЕГОРИИ ТРАТ =============");
+
+            System.out.println("\n\nСАМАЯ ДОРОГАЯ ТРАТА: ");
+            System.out.println(Analyzer.getMostExpensive(transactions));
+        } catch (Exception e) {
+            System.err.print("Ошибка выполнения программы: ");
+            e.printStackTrace();
+            System.exit(1);
         }
-        return result;
-    }
-
-    public static Map<String, Long> groupByCategory(List<Transaction> transactions) {
-        return transactions.stream().collect(Collectors.groupingBy(Transaction::getCategory, Collectors.counting()));
-    }
-
-    public static Transaction getMostExpensive(List<Transaction> transactions) {
-        return transactions.stream().max(Comparator.comparing(Transaction::getAmount)).get();
     }
 }
